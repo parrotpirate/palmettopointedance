26422c5a18233920861d9f1683c1f3b7c5499006
Stephen Parker <stephen.parker@q4launch.com>
2022-06-27 18:02:34 -0400

    Remove test field

--- app/carbon_fields.php
+++ app/carbon_fields.php
@@ -5,15 +5,6 @@ namespace App;
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
-function crb_attach_theme_options()
-{
-    Container::make('theme_options', __('Theme Options'))
-        ->add_fields(array(
-            Field::make('text', 'crb_text', 'Text Field'),
-        ));
-}
-add_action('carbon_fields_register_fields', __NAMESPACE__ . '\\crb_attach_theme_options');
-
 function crb_load()
 {
     \Carbon_Fields\Carbon_Fields::boot();
